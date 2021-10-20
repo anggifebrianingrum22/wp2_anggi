82cc21cf46bb06386aed4a66dfc224d33839e680
anggifebrianingrum22 <92362323+anggifebrianingrum22@users.noreply.github.com>
2021-10-20 10:49:39 +0700

    Pertemuan 2
    Pertemuan 2

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Anggi Febrianingrum
+ Saya tingga di daerah Jaticempaka
+ olah raga yang saya sukai adalah
+ Berenang";
+    }
+}
