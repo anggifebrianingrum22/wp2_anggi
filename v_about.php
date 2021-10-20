dc560e04a1c72e8ae3b01f67859b4e2e2d8640ef
anggifebrianingrum22 <92362323+anggifebrianingrum22@users.noreply.github.com>
2021-10-20 11:11:57 +0700

    Pertemuan 4
    Pertemuan 4

---
+++ v_about.php
@@ -0,0 +1,27 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Anggi </li>
+        <li>Nama Belakang : Febrianingrum</li>
+        <li>Nama Lengkap : Anggi Febrianingrum</li>
+    </ul>
+    <br>
+
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Jalan Setia 1,JatiCempaka</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li>Ngawi</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Jogging</li>
+    </ul>
+</section>
